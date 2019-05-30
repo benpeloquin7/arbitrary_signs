748ff98f73fb3448b5d62c1a35395931ec418e1c
Ben Peloquin <benjamin.peloquin@gmail.com>
2019-05-30 12:55:16 -0700

    cleanup

--- arbitrary_signs/correlations.py
+++ arbitrary_signs/correlations.py
@@ -1,7 +1,6 @@
-"""correlations"""
+"""correlations.py"""
 
 from collections import defaultdict
-import itertools as it
 from Levenshtein import distance as levenshtein
 import logging
 import numpy as np
@@ -109,69 +108,6 @@ def get_correlations(dists1, dists2):
     return np.corrcoef(dists1, dists2)
 
 
-# def get_vocab_sample(model, n=500):
-#     """
-#
-#     Parameters
-#     ----------
-#     model: dict
-#         Keys are words, values are vector repr.
-#     n: int [Default: 500]
-#         Number of words to sample.
-#
-#     Returns
-#     -------
-#     tuple (np.array, np.array, np.array)
-#         Vector, words, random ordered words.
-#
-#     """
-#     vocab_words_1 = np.array(list(model.keys()))
-#     vocab_words_2 = np.array(list(model.keys()))
-#     vector_repr = np.array(list(model.values()))
-#
-#     idxs = np.random.choice(range(len(vocab_words_1)), size=n)
-#     words_sample = vocab_words_1[idxs]
-#     words_sample_random = vocab_words_2[idxs]
-#     vector_sample = vector_repr[idxs]
-#     np.random.shuffle(words_sample_random)
-#     # Basic check on randomization
-#     assert words_sample[0] != words_sample_random[0]
-#     return vector_sample, words_sample, words_sample_random
-#
-#
-# def get_pairwise_distances(items, dist_fn=lambda x: x):
-#     pairs = list(it.product(items, items))
-#     final_pairs = []
-#     dists = []
-#     for a, b in pairs:
-#         dist = dist_fn(a, b)
-#         # Note (BP): You may need to complicate this...
-#         if dist == 0:
-#             continue
-#         dists.append(dist)
-#         final_pairs.append((a, b))
-#     return dists, final_pairs
-#
-#
-# def get_distance_correlations(model, n=500):
-#     vals, words, random_words = get_vocab_sample(model, n)
-#     val_dists, _ = get_pairwise_distances(vals, cosine)
-#     word_dists, _ = get_pairwise_distances(words, levenshtein)
-#     random_word_dists, _ = get_pairwise_distances(random_words, levenshtein)
-#     return np.corrcoef(val_dists, word_dists)[0][1], \
-#            np.corrcoef(val_dists, random_word_dists)[0][1]
-#
-#
-# def run_distance_correlations(model, n_sims=100, n_sample=100):
-#     real_corrs = []
-#     random_corrs = []
-#     for _ in tqdm.tqdm(range(n_sims)):
-#         real_corr, random_corr = get_distance_correlations(model, n_sample)
-#         real_corrs.append(real_corr)
-#         random_corrs.append(random_corr)
-#     return np.array(real_corrs), np.array(random_corrs)
-
-
 if __name__ == "__main__":
     import argparse
 
@@ -226,36 +162,3 @@ if __name__ == "__main__":
     logging.info("Dumping simulations to {}".format(dists_fp))
     with open(dists_fp, "wb") as fp:
         pickle.dump(dists, fp)
-
-
-
-
-
-
-    import pdb; pdb.set_trace();
-
-
-
-
-
-    # real_corrs, random_corrs = \
-    #     run_distance_correlations(model, n_samples, sample_size)
-    #
-    # # Cacheing
-    # if args.cache_model:
-    #     model_fp = \
-    #         os.path.join(args.out_dir,
-    #                      "{}_model.pickle".format(args.language_ext))
-    #     logging.info("Cacheing model to {}".format(model_fp))
-    #     with open(model_fp, "wb") as fp:
-    #         pickle.dump(model, fp)
-    #
-    # real_corrs_fp = \
-    #     os.path.join(args.out_dir,
-    #                  "{}_real_correlations.npy".format(args.language_ext))
-    # np.save(real_corrs_fp, real_corrs)
-    #
-    # random_corrs_fp = \
-    #     os.path.join(args.out_dir,
-    #                  "{}_random_correlations.npy".format(args.language_ext))
-    # np.save(random_corrs_fp, random_corrs)
